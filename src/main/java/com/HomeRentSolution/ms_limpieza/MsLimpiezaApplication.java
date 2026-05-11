3135e605eee0a2b16d7e9428b9cb5c1da8f35424
Naoshinaps <n.paillallao.17@gmail.com>
2026-05-11 02:33:05 -0400

    base de microservicio Limpieza

---
+++ src/main/java/com/HomeRentSolution/ms_limpieza/MsLimpiezaApplication.java
@@ -0,0 +1,13 @@
+package com.HomeRentSolution.ms_limpieza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsLimpiezaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsLimpiezaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/HomeRentSolution/ms_limpieza/MsLimpiezaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.HomeRentSolution.ms_limpieza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsLimpiezaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

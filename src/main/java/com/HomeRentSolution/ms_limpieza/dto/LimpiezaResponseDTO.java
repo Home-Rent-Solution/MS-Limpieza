8bc9586a778431e190136571d14f0ee7071d5ba2
Naoshinaps <n.paillallao.17@gmail.com>
2026-05-14 09:57:44 -0400

    se crea la clase limpieza response DTO y sus variables

---
+++ src/main/java/com/HomeRentSolution/ms_limpieza/dto/LimpiezaResponseDTO.java
@@ -0,0 +1,15 @@
+package com.HomeRentSolution.ms_limpieza.dto;
+
+import com.HomeRentSolution.ms_limpieza.model.EstadoLimpieza;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class LimpiezaResponseDTO {
+    private Long idLimpieza;
+    private Long idPropiedad;
+    private Long idReserva;
+    private LocalDateTime fechaProgramada;
+    private EstadoLimpieza estado;
+}

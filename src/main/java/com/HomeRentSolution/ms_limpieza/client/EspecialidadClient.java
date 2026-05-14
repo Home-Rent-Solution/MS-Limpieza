2b18fee840fbb9aab596ace9d9b252e1d61200c5
Naoshinaps <n.paillallao.17@gmail.com>
2026-05-14 10:02:35 -0400

    EndPoints listos

--- src/main/java/com/HomeRentSolution/ms_limpieza/client/EspecialidadClient.java
+++ src/main/java/com/HomeRentSolution/ms_limpieza/client/EspecialidadClient.java
@@ -1,4 +1,18 @@
 package com.HomeRentSolution.ms_limpieza.client;
 
+import com.HomeRentSolution.ms_limpieza.dto.LimpiezaRequestDTO;
+import com.HomeRentSolution.ms_limpieza.dto.LimpiezaResponseDTO;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 public interface EspecialidadClient {
+
+    @PostMapping("/api/limpieza/agendar")
+    LimpiezaResponseDTO agendarLimpieza(@RequestBody LimpiezaRequestDTO dto);
+
+    @PutMapping("/api/limpieza/{id}/cancelar")
+    void cancelarLimpieza(@PathVariable("id") Long idLimpieza);
+
 }

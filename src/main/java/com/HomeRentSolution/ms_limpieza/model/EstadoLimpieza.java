5f2c27aa8816a45e2501aa2951ca09eab15d4af4
Naoshinaps <n.paillallao.17@gmail.com>
2026-05-14 09:52:45 -0400

    se crea un enum para el estado de limpieza y se termina de agregar las variables a la clase limpieza

---
+++ src/main/java/com/HomeRentSolution/ms_limpieza/model/EstadoLimpieza.java
@@ -0,0 +1,8 @@
+package com.HomeRentSolution.ms_limpieza.model;
+
+public enum EstadoLimpieza {
+    PENDIENTE,
+    EN_PROCESO,
+    COMPLETADA,
+    CANCELADA
+}
--- src/main/java/com/HomeRentSolution/ms_limpieza/model/Limpieza.java
+++ src/main/java/com/HomeRentSolution/ms_limpieza/model/Limpieza.java
@@ -1,4 +1,25 @@
 package com.HomeRentSolution.ms_limpieza.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
 public class Limpieza {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idLimpieza;
+
+    private Long idPropiedad;
+    private Long idReserva;
+
+    private LocalDateTime fechaProgramada;  // cuando se agenda (fin de reserva)
+    private LocalDateTime fechaRealizada;   // cuando se completó
+
+    @Enumerated(EnumType.STRING)
+    private EstadoLimpieza estado;
 }

acc753808a1dc1d1147f33e576d4e4ccffe70915
Naoshinaps <n.paillallao.17@gmail.com>
2026-05-16 16:38:26 -0400

    cambier los import porque los puse mal XD

--- src/main/java/com/HomeRentSolution/ms_limpieza/model/Limpieza.java
+++ src/main/java/com/HomeRentSolution/ms_limpieza/model/Limpieza.java
@@ -2,7 +2,7 @@ package com.HomeRentSolution.ms_limpieza.model;
 
 import jakarta.persistence.*;
 import lombok.Data;
-import org.springframework.data.annotation.Id;
+
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;

58230b6bb66e4dffa3d7c7a69955b36b89ebc28c
Naoshinaps <n.paillallao.17@gmail.com>
2026-06-19 15:41:03 -0400

    se agrego un list en el repository

--- src/main/java/com/HomeRentSolution/ms_limpieza/repository/LimpiezaRepository.java
+++ src/main/java/com/HomeRentSolution/ms_limpieza/repository/LimpiezaRepository.java
@@ -5,11 +5,11 @@ import com.HomeRentSolution.ms_limpieza.model.Limpieza;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
-
-
+import java.util.Optional;
 public interface LimpiezaRepository extends JpaRepository<Limpieza, Long> {
 
-
     List<Limpieza> findByEstadoLimpieza(EstadoLimpieza estadoLimpieza);
+
+    Optional<Limpieza> findByIdReserva(Long idReserva);
 
 }

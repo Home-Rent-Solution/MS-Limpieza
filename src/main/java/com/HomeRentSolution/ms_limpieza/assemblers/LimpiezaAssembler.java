a5ab133ae7a42382edaf64125cb599f651dbd72e
Naoshinaps <n.paillallao.17@gmail.com>
2026-06-19 01:35:13 -0400

    se crea nueva carpeta y clase assemblers

---
+++ src/main/java/com/HomeRentSolution/ms_limpieza/assemblers/LimpiezaAssembler.java
@@ -0,0 +1,33 @@
+package com.HomeRentSolution.ms_limpieza.assemblers;
+
+import com.HomeRentSolution.ms_limpieza.controller.LimpiezaV2Controller;
+import com.HomeRentSolution.ms_limpieza.dto.LimpiezaResponseDTO;
+import com.HomeRentSolution.ms_limpieza.model.Limpieza;
+import org.springframework.hateoas.server.mvc.RepresentationModelAssemblerSupport;
+import org.springframework.stereotype.Component;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+
+@Component
+public class LimpiezaAssembler {
+
+    public LimpiezaAssembler() {
+        super(LimpiezaV2Controller.class, LimpiezaResponseDTO.class);
+    }
+
+    @Override
+    public LimpiezaResponseDTO toModel(Limpieza entidad) {
+        LimpiezaResponseDTO dto = new LimpiezaResponseDTO();
+        dto.setIdLimpieza(entidad.getIdLimpieza());
+        dto.setIdReserva(entidad.getIdReserva());
+        dto.setIdPropiedad(entidad.getIdPropiedad());
+        dto.setFechaProgramada(entidad.getFechaProgramada());
+        dto.setFechaRealizada(entidad.getFechaRealizada());
+        dto.setEstadoLimpieza(entidad.getEstadoLimpieza());
+        dto.setObservaciones(entidad.getMotivo());
+
+        // Inyección del enlace autodescriptivo de HATEOAS (Self Link)
+        dto.add(linkTo(methodOn(LimpiezaV2Controller.class).obtenerPorId(entidad.getIdLimpieza())).withSelfRel());
+        return dto;
+    }
+}

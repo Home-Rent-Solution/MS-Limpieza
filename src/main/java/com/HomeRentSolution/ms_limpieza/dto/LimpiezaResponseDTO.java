357ba358090df70e8f3fac762e3c8a7e51e8b8b6
Naoshinaps <n.paillallao.17@gmail.com>
2026-06-19 01:32:48 -0400

    se modifican variables en responseDTO

--- src/main/java/com/HomeRentSolution/ms_limpieza/dto/LimpiezaResponseDTO.java
+++ src/main/java/com/HomeRentSolution/ms_limpieza/dto/LimpiezaResponseDTO.java
@@ -1,16 +1,16 @@
 package com.HomeRentSolution.ms_limpieza.dto;
 
 import com.HomeRentSolution.ms_limpieza.model.EstadoLimpieza;
-import jakarta.persistence.Column;
 import lombok.Data;
+import org.springframework.hateoas.RepresentationModel;
 
 import java.time.LocalDateTime;
 
 @Data
-public class LimpiezaResponseDTO {
+public class LimpiezaResponseDTO extends RepresentationModel<LimpiezaResponseDTO> {
     private Long idLimpieza;
-    private Long idPropiedad;
     private Long idReserva;
+    private Long idPropiedad;
     private LocalDateTime fechaProgramada;
     private LocalDateTime fechaRealizada;
     private EstadoLimpieza estadoLimpieza;
